4af8561a4ef55e4e34c445f2aa8cc5eefadcc231
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2023-03-06 09:12:24 -0300

    Anotações em utils da classe Tarefa

--- src/tarefa/utils.py
+++ src/tarefa/utils.py
@@ -1,12 +1,16 @@
+"""Procedimentos utilitários para o processador"""
+
 import pandas as pd
 
-def bool_tostring(valor):
+def bool_tostring(valor: bool) -> str:
+    """Converte um valor booleano para string."""
     if valor:
         return 'Sim'
     else:
         return 'Não'
 
-def valor_tostring(valor):
+def valor_tostring(valor) -> str:
+    """Converte um valor da base de dados para string."""
     if pd.isna(valor):
         return '(nenhum)'
     else:
