a52da7ef94f4b5026dd0abd7623339b256a37657
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2023-05-25 14:40:19 -0300

    Automação do Portal CNIS

---
+++ src/navegador/cnis.py
@@ -0,0 +1,101 @@
+## Codificado por Douglas Rodrigues de Almeida.
+## Março de 2023
+
+"""Automatizador do CNIS."""
+
+import time
+from os import path
+from .navegador import Navegador
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+URL_CNIS_INTRANET = 'https://geridinss.dataprev.gov.br:8443/cas/login?service=http://pcnisapr02.prevnet/cnis/index.html'
+
+class Cnis(Navegador):
+    """Classe do Automatizador do Cnis."""
+    def __init__(self) -> None:
+        super().__init__()
+
+    def abrir(self):
+        """Abre o CNIS."""
+        self.driver.get(URL_CNIS_INTRANET)
+        #Aguarda autenticação
+        time.sleep(10)
+
+    def abrir_cpf(self, cpf: str) -> None:
+        """"""
+        #drv = form
+        drv = self.driver
+
+        WebDriverWait(self.driver, timeout=7).until(EC.visibility_of_element_located((By.ID, 'formNovo:opcoesConsulta:inputNumeroConsulta')))
+        campo = drv.find_element(By.ID, 'formNovo:opcoesConsulta:inputNumeroConsulta')
+        campo.click()
+        campo.send_keys(cpf)
+        drv.find_element(By.ID, 'formNovo:acaoPesquisar').click()
+        self.aguardar_processamento()
+        try:
+            WebDriverWait(self.driver, timeout=50).until(EC.presence_of_element_located((By.ID, 'formNovo:lista:0:botaoAcao'))).click()
+            #WebDriverWait(self.driver, timeout=50).until(EC.visibility_of_element_located((By.ID, 'formNovo:lista:0:botaoAcao'))).click()
+        except Exception as erro:
+            return
+        self.aguardar_processamento()
+
+    def aguardar_processamento(self) -> None:
+        """Espera pelo encerramento da tela 'Aguardando processamento'"""
+        espera = WebDriverWait(self.driver, 60, poll_frequency=1)
+        #Aguarda pela invisibilidade do elemento
+        espera.until(EC.invisibility_of_element((By.CLASS_NAME, 'blockUI')))
+
+    def pesquisar_nit_decpf(self, protocolo: str, cpf: str) -> str:
+        """Retorna um NIT relacionado ao CPF informado."""
+        drv = self.driver
+
+        #Clicar no menu e reabrir a tela de pesquisa.
+        drv.find_element(By.ID, 'itemMenuConsultasDoSistema').click()
+        drv.find_element(By.ID, 'formMenu:identificarFiliadoConsultaExtratoCidadao').click()
+        WebDriverWait(self.driver, timeout=5).until(EC.visibility_of_element_located((By.ID, 'formNovo:opcoesConsulta:inputNumeroConsulta')))
+        campo = drv.find_element(By.ID, 'formNovo:opcoesConsulta:inputNumeroConsulta')
+        campo.click()
+        campo.send_keys(cpf)
+        drv.find_element(By.ID, 'formNovo:acaoPesquisar').click()
+        self.aguardar_processamento()
+        tabela = drv.find_element(By.ID, 'formNovo:lista_data')
+        nit = tabela.find_elements(By.TAG_NAME, 'td')[1].text
+        return f'{nit[0:3]}.{nit[3:8]}.{nit[8:10]}-{nit[10]}'
+
+    def pesquisar_ben_ativos(self, protocolo: str, cpf: str, especies: list[str]) -> bool:
+        """Verifica se existe BEN ativo para o CPF informado e gera seu relatório."""
+        STATUS_ATIVO = '0 - ATIVO'
+        drv = self.driver
+
+        #Clicar no menu e reabrir a tela de pesquisa.
+        drv.find_element(By.ID, 'itemMenuConsultasDoSistema').click()
+        drv.find_element(By.ID, 'formMenu:historicoBeneficio').click()
+        
+
+        #Pesuisar o CPF especificado.
+        self.abrir_cpf(cpf)
+        self.tarefa = protocolo
+        try:
+            WebDriverWait(self.driver, timeout=4).until(EC.presence_of_element_located((By.ID, 'exibirHistoricoBeneficios')))
+        except:
+            if len((campos := drv.find_elements(By.CLASS_NAME, 'ui-messages-error-detail'))) > 0:
+                if campos[0].text == 'Filiado sem benefícios':
+                    return False
+                
+        #Raspagem no histórico de benefícios
+        form = drv.find_element(By.ID, 'exibirHistoricoBeneficios')           
+        campo = form.find_element(By.ID, 'exibirHistoricoBeneficios:list:tbody_element')
+        for item in campo.find_elements(By.TAG_NAME, 'tr'):
+            nb = item.find_elements(By.TAG_NAME, 'td')[0].text.strip()
+            status = item.find_elements(By.TAG_NAME, 'td')[3].text.strip()
+            especie = item.find_elements(By.TAG_NAME, 'td')[4].text.strip()[2]
+            if (status == STATUS_ATIVO and not especie in especies):
+                form.find_element(By.ID, 'exibirHistoricoBeneficios:Imprimir').click()
+                arquivo_gerado = path.join(self.dir_downloads, 'exibirHistoricoBeneficios.xhtml.pdf')
+                arquivo_novo = path.join(self.dir_downloads, f'{self.tarefa} - Analise.pdf')
+                self.manipular_pdf(arquivo_gerado, arquivo_novo)
+                return True
+        return False
