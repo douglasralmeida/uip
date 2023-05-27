e8bb21ad1efe23dd8459a65551ec7a40f54f2ab9
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2023-05-27 09:55:10 -0300

    implementação da tarefa de salário maternidade

--- src/tarefa/__init__.py
+++ src/tarefa/__init__.py
@@ -3,5 +3,6 @@ from .tarauxacidente import TarefaAuxilioAcidente
 from .tarauxincap import TarefaAuxilioIncapacidade
 from .tariir import TarefaIsencaoIR
 from .tarpsalmat import TarefaProrrogacaoSalMaternidade
+from .tarsm import TarefaSalMaternidade
 
-__all__ = ["Tarefa", "TarefaAuxilioAcidente", "TarefaAuxilioIncapacidade", "TarefaIsencaoIR", "TarefaProrrogacaoSalMaternidade"]
+__all__ = ["Tarefa", "TarefaAuxilioAcidente", "TarefaAuxilioIncapacidade", "TarefaIsencaoIR", "TarefaProrrogacaoSalMaternidade", "TarefaSalMaternidade"]
