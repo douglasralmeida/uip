8c165124d48071928bbee42f5ba9e263ff4608d2
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2023-09-20 18:38:09 -0300

    Adiciona exceções na conversão texto-pdf

--- src/conversor/conversor.py
+++ src/conversor/conversor.py
@@ -1,15 +1,18 @@
 import os
-from pypdf import PdfReader
+from pypdf import PdfReader, PdfWriter
+from PIL import Image
 from variaveis import Variaveis
 
 class Conversor:
-    def processar(self, origem, destino, relat, lista):
+    def processar(self, origem: str, destino: str, relat: str, lista: list[str]) -> bool:
         self.relat = relat
         self.lista = lista
         if origem == 'pdf' and destino == 'txt':
-            self.converter_pdf_txt()
+            return self.converter_pdf_txt()
+        else:
+            return False
 
-    def converter_pdf_txt(self):
+    def converter_pdf_txt(self) -> bool:
         nome_arquivo_pdf = ''
         nome_arquivo_txt = ''
         texto = ''
@@ -17,11 +20,22 @@ class Conversor:
         for tarefa in self.lista:
             nome_arquivo_pdf = os.path.join(Variaveis.obter_pasta_pdf(), f'{tarefa} - {self.relat}.pdf')
             nome_arquivo_txt = os.path.join(Variaveis.obter_pasta_entrada(), f'{tarefa} - {self.relat}.txt')
-            print(f'Processando tarefa {tarefa}...')
-            with open(nome_arquivo_pdf, 'rb') as arquivo_pdf:
-                leitor = PdfReader(arquivo_pdf)
-                for pagina in leitor.pages:
-                    texto += pagina.extract_text()
-                    texto += '\n'
-            with open(nome_arquivo_txt, 'w') as arquivo_txt:
-                arquivo_txt.write(texto)
+            print(f"Processando tarefa {tarefa}...")
+            try:
+                with open(nome_arquivo_pdf, 'rb') as arquivo_pdf:
+                    leitor = PdfReader(arquivo_pdf)
+                    texto = leitor.pages[0].extract_text()
+                    if len(leitor.pages) > 1:
+                        texto += '\n'
+                        texto += leitor.pages[1].extract_text()
+                        texto += '\n'
+            except OSError as err:
+                print(f"Erro ao abrir arquivo PDF {tarefa} - {self.relat}.pdf: {err.strerror}.")
+                return False
+            try:
+                with open(nome_arquivo_txt, 'w') as arquivo_txt:
+                    arquivo_txt.write(texto)
+            except OSError as err:
+                print(f"Erro ao salvar arquivo texto {tarefa} - {self.relat}.txt: {err.strerror}.")
+                return False
+        return True
