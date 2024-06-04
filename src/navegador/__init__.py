e47804c7e290411481df959b745992f5bd9fae90
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2024-06-03 22:09:49 -0300

    Automação para GERID GPA

--- src/navegador/__init__.py
+++ src/navegador/__init__.py
@@ -1,8 +1,9 @@
 from .cnis import Cnis
 from .get import Get
+from .gpa import Gpa
 from .pmfagenda import Pmfagenda
 from .sd import SD
 from .sibe import Sibe
 from .locaispm import LocaisPM
 
-__all__ = ["Cnis", "Get", "LocaisPM", "Pmfagenda", "SD", "Sibe"]
+__all__ = ["Cnis", "Get", "LocaisPM", "Pmfagenda", "SD", "Sibe", 'Gpa']
